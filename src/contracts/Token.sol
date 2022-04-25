82762caddea46a1c31c2670fd83a7a2e02a9de19
CJ <cw@cjwilliamstech.com>
2022-04-24 20:42:57 -1000

    Added new token functions
    new functions

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -1,6 +1,9 @@
 pragma solidity ^0.5.0;
 
+import "openzeppelin-solidity/contracts/math/SafeMath.sol";
+
 contract Token {
+  using SafeMath for uint;
   string public name= "DApp Token";
   string public symbol = "DAPP";
   uint256 public decimals = 18;
@@ -12,7 +15,7 @@ contract Token {
       balanceOF[msg.sender] = totalSupply;
   }
 
-  function transfer(address_to, uint256_value) public returns (bool success) {
+  function transfer(address _to, uint256 _value) public returns (bool success) {
     balanceOF[msg.sender] = balanceOF[msg.sender].sub(_value);
     balanceOF[_to] =balanceOF[_to].add(_value);
     return true;
--- test/Token.test.js
+++ test/Token.test.js
@@ -1,12 +1,18 @@
+const { default: Web3 } = require("web3");
+
 const Token = artifacts.require("./Token");
 
 require("chai").use(require("chai-as-promised")).should();
 
-contract("Token", ([deployer]) => {
+const tokens = (n) => {
+  return new Web3.utils.isBN(Web3.utils.toWei(n.toString(), "ether"));
+};
+
+contract("Token", ([deployer, receiver]) => {
   const name = "DApp Token";
   const symbol = "DAPP";
   const decimals = "18";
-  const totalSupply = "1000000000000000000000000";
+  const totalSupply = tokens(1000000).toString();
   let token;
 
   beforeEach(async () => {
@@ -31,12 +37,35 @@ contract("Token", ([deployer]) => {
 
     it("tracks the total supply", async () => {
       const result = await token.totalSupply();
-      result.toString().should.equal(totalSupply);
+      result.toString().should.equal(totalSupply.toString());
     });
 
     it("assigns the total supply to the deployer", async () => {
       const result = await token.balanceOF(deployer);
-      result.toString().should.equal(totalSupply);
+      result.toString().should.equal(totalSupply.toString());
+    });
+  });
+
+  describe("sending tokens", () => {
+    it("transfers token balances", async () => {
+      let balanceOF;
+      //Before transfer
+      balanceOF = await token.balanceOF(deployer);
+      console.log("deployer balance before transfer", balanceOF.toString);
+      balanceOF = await token.balanceOF(receiver);
+      console.log("receiver balance before transfer", balanceOF.toString);
+
+      // Transfer
+
+      await token.transfer(receiver, tokens(100), {
+        from: deployer,
+      });
+
+      // After transfer
+      balanceOF = await token.balanceOF(deployer);
+      console.log("deployer balance after transfer", balanceOF.toString);
+      balanceOF = await token.balanceOF(receiver);
+      console.log("receiver blance after transfer", balanceOF.toString);
     });
   });
 });
